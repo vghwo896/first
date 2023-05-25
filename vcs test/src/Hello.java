460688ecbe67933e03c472c38bf0be4c0c436a7b
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-25 10:08:54 +0900

    첫번째 문장 작업.

--- vcs test/src/Hello.java
+++ vcs test/src/Hello.java
@@ -1,11 +1,13 @@
-
 public class Hello {
 	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
 		System.out.println("Hello world");
 
 		System.out.println("변경");
 
 		System.out.println("세번째 커밋");
+
 		// 주석
 	}
 }
+
